04b52b0589759b778a169b3fc651c2a01096aae4
kimura_kouichirou <814.kimura86@gmail.com>
2019-09-07 16:31:09 +0900

    migrationの作成

--- database/migrations/2019_09_07_072302_create_datas_table.php
+++ database/migrations/2019_09_07_072302_create_datas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateDatasTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('datas', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('token', 60);
-            $table->rememberToken();
+            $table->unsignedBigInteger('column_id');
+            $table->mediumText('value')->nullable();
             $table->timestamps();
+
+            $table->foreign('column_id')->references('id')->on('columns');
         });
     }
 
@@ -32,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('datas');
     }
 }
